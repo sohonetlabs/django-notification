1b385c01fca9e1a3ac3bf4f8d2e5cca26c0b2fa9
Mathieu Pillard <m.pillard@liberation.fr>
2010-12-16 16:50:30 +0100

    Add another helper to get notice types

--- notification/templatetags/noticetype_tags.py
+++ notification/templatetags/noticetype_tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 def get_noticetypes_startingwith(txt):
     types = NoticeType.objects.filter(label__startswith=txt).values_list('pk', flat=True)
     return u",".join([str(i) for i in types])
+    
+@register.simple_tag
+def get_noticetypes_in(labels):
+    labels = labels.split(",")
+    types = NoticeType.objects.filter(label__in=labels).values_list('pk', flat=True)
+    return u",".join([str(i) for i in types])
