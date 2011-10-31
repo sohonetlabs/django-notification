bd5b12c6a8e8cbcdc38be90df14672ec1ae930d7
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-10-31 11:31:56 +0100

    added clean_notifications command with tests

---
+++ notification/management/commands/clean_notifications.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+from time import time
+from datetime import datetime
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from notification.models import Notice
+
+
+class Command(BaseCommand):
+    """Remove old queue item"""
+    help = __doc__
+
+    option_list = BaseCommand.option_list + (
+        make_option('--dry-run',
+            action='store_true',
+            dest='dryrun',
+            default=False,
+            help='Report what will be deleted without actually doing it'),
+        )
+
+    def handle(self, *args, **options):
+        dryrun = options['dryrun']
+        limit = time() - settings.NOTIFICATION_MAX_AGE
+        limit = datetime.fromtimestamp(limit)
+        notices = Notice.objects.filter(added__lte=limit)
+        if dryrun:
+            for notice in notices:
+                print '%s pk=%s\n' % (notice, notice.pk)
+            count = notices.count()
+            print 'Total count of notice to be deleted %s' % count
+        else:
+            notices.delete()
+
+
---
+++ notification/tests/__init__.py
@@ -0,0 +1,1 @@
+from test_clean_notifications import *
