75867589ad63078cba8271d228920a66a02833b0
Mathieu Pillard <m.pillard@liberation.fr>
2012-06-25 13:14:29 +0200

    Follow django documentation regarding locals in management commands
    Make sure the default LANGUAGE_CODE is honored in emit_notices,
    because django set the hardcoded 'en-us' by default if we don't
    do anything, which is usually wrong for emit_notices.

--- notification/management/commands/emit_notices.py
+++ notification/management/commands/emit_notices.py
@@ -2,6 +2,8 @@
 import logging
 
 from django.core.management.base import NoArgsCommand
+from django.utils import translation
+from django.conf import settings
 
 from notification.engine import send_all
 
@@ -9,7 +11,11 @@ class Command(NoArgsCommand):
     help = "Emit queued notices."
     
     def handle_noargs(self, **options):
+        translation.activate(settings.LANGUAGE_CODE)
+
         logging.basicConfig(level=logging.DEBUG, format="%(message)s")
         logging.info("-" * 72)
         send_all()
+
+        translation.deactivate()
     
